89de43b6d8f3c52e31ddf3b9c752c19d5d77e918
Christopher Wilbar <cwilbar@alumni.nd.edu>
2021-01-24 21:20:56 -0600

    Update test_myrepo.py

--- tests/test_myrepo.py
+++ tests/test_myrepo.py
@@ -3,3 +3,7 @@ from mylibrary import dummy
 def test_func():
     result = dummy.dummy_func()
     assert result == 1
+    
+def test_subtract():
+    result = dummy.subtract(5,3)
+    assert result = 2
